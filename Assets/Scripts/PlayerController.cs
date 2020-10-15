5e84a24b9c7190ca3514aa36dcc55b7a377ee06e
Edgar <edgaradrian.mejiarazo@gmail.com>
2020-10-15 00:18:18 -0400

    Player Controller
    Movement of player added and colliding. Top Down assets added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // "Public" variables
+
+    [SerializeField] private float speed = 10.0f;
+
+
+
+
+    //Private Variables
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    //Physics
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+
+        rBody.velocity = new Vector2(horiz * speed, verti * speed);
+    }
+}
