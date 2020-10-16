e2dd25988ea04e95542cc5e529c25bea2704d098
Otaku2932 <c_lapis4@hotmail.com>
2020-10-15 22:31:16 -0400

    camera added, test enemy(placeholder) added + basic enemy movement must be managed within moving distance

---
+++ Assets/Scripts/ThiefController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefController : MonoBehaviour
+{
+    // "Public" variables
+
+    [SerializeField] private bool dirRight = true;
+    [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float distance = 4.0f;
+    private float initialx;
+
+
+
+
+    //Private Variables
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        initialx = rBody.position.x;
+    }
+
+    //Physics
+    private void FixedUpdate()
+    {
+        if (dirRight)
+            rBody.velocity = new Vector2(speed * 10f, 0);
+        else
+            rBody.velocity = new Vector2(-speed * 10f, 0);
+
+        if (rBody.position.x >= initialx + distance)
+        {
+            dirRight = false;
+        }
+
+        if (rBody.position.x <= initialx - distance)
+        {
+            dirRight = true;
+        }
+    }
+}
