249dfabc0e80463d67f83dfe02008da0aa4a8da4
Franz <fcadien1@my.centennialcollege.ca>
2020-11-04 14:45:20 -0500

    Added Attack Mechanic

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private bool hasEnemy = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            hasEnemy = true;
+
+        if(hasEnemy && Input.GetMouseButtonDown(0))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            hasEnemy = true;
+        if (hasEnemy && Input.GetMouseButtonDown(0))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            hasEnemy = false;
+    }
+  
+}
