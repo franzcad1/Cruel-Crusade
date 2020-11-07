4c23eba54a873f04984a86ddac8478abe2b83480
Otaku2932 <c_lapis4@hotmail.com>
2020-11-06 23:24:30 -0500

    merge with main and simple enemy prefabs

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject knight;
+    [SerializeField] private GameObject rogue;
+    [SerializeField] private GameObject characterSelection;
+    [SerializeField] private GameObject cinemachine;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterSelection = GameObject.Find("CharacterSelection");
+       
+
+        if (characterSelection.GetComponent<CharacterSelection>().characterChoice == "rogue")
+        {
+            player = Instantiate(rogue, new Vector2(this.transform.position.x, this.transform.position.y), transform.rotation);
+            cinemachine.GetComponent<CinemachineVirtualCamera>().Follow = player.transform;
+        }
+        else if (characterSelection.GetComponent<CharacterSelection>().characterChoice == "knight")
+        {
+            player = Instantiate(knight, new Vector2(this.transform.position.x, this.transform.position.y), transform.rotation);
+            cinemachine.GetComponent<CinemachineVirtualCamera>().Follow = player.transform;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Canvas gameHUD;
+    private bool win = false;
+    public Text timetext;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameHUD);
+        startTime = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win)
+            return;
+
+        float t = Time.timeSinceLevelLoad - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        var minuteTest = ((int)t / 60);
+        string seconds = (t % 60).ToString("f2");
+
+        
+
+        if (minuteTest == 0)
+            timetext.text = seconds;
+        else
+            timetext.text = minutes + "." + seconds;
+
+    }
+
+    public void Finish()
+    {
+        win = true;
+        timetext.color = Color.yellow;
+    }
+}
