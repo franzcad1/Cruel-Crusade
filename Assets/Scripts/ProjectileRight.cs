878c5cd7fdce64dcabdae91f9873e5f053448d72
Otaku2932 <c_lapis4@hotmail.com>
2020-11-06 21:27:10 -0500

    pull from main with a few scripts

---
+++ Assets/Scripts/ProjectileRight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileRight : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
