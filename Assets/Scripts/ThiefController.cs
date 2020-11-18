01e54863db046da9f26e1cc354e30767395f7281
pinct <pinctcourneya@gmail.com>
2020-11-18 18:53:20 -0500

    Fixed thief movement to follow player

--- Assets/Scripts/ThiefController.cs
+++ Assets/Scripts/ThiefController.cs
@@ -6,10 +6,12 @@ public class ThiefController : MonoBehaviour
 {
     // "Public" variables
 
-    [SerializeField] private bool dirRight = true;
+    //[SerializeField] private bool dirRight = true;
     [SerializeField] private float speed = 2.0f;
-    [SerializeField] private float distance = 4.0f;
+    //[SerializeField] private float distance = 4.0f;
     private float initialx;
+    [SerializeField] private bool playerInRange = false;
+    [SerializeField] private GameObject player;
 
 
 
@@ -22,24 +24,53 @@ public class ThiefController : MonoBehaviour
     {
         rBody = GetComponent<Rigidbody2D>();
         initialx = rBody.position.x;
+        if (GameObject.FindGameObjectWithTag("Knight") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Knight");
+        }
+        else if (GameObject.FindGameObjectWithTag("Rogue") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Rogue");
+        }
     }
 
     //Physics
     private void FixedUpdate()
     {
-        if (dirRight)
-            rBody.velocity = new Vector2(speed * 10f, 0);
-        else
-            rBody.velocity = new Vector2(-speed * 10f, 0);
+        //if (dirRight)
+        //    rBody.velocity = new Vector2(speed * 10f, 0);
+        //else
+        //    rBody.velocity = new Vector2(-speed * 10f, 0);
 
-        if (rBody.position.x >= initialx + distance)
+        //if (rBody.position.x >= initialx + distance)
+        //{
+        //    dirRight = false;
+        //}
+
+        //if (rBody.position.x <= initialx - distance)
+        //{
+        //    dirRight = true;
+        //}
+
+        if (playerInRange)
         {
-            dirRight = false;
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed);
         }
+    }
 
-        if (rBody.position.x <= initialx - distance)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Knight" || collision.gameObject.tag == "Rogue")
         {
-            dirRight = true;
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Knight" || collision.gameObject.tag == "Rogue")
+        {
+            playerInRange = false;
         }
     }
 }
