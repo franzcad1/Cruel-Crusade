06757b22df3b2c4d4d49913b7a12b4f84d982dd2
Otaku2932 <c_lapis4@hotmail.com>
2020-11-20 20:14:09 -0500

    overwrite from enemy_changes branch

---
+++ Assets/Scripts/AOE.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOE : MonoBehaviour
+{
+    public Transform firePoint;
+    [SerializeField] private GameObject aoeP;
+
+    public float aoeSpread = 4f;
+    public int density = 6;
+    public float interval = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Fire());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fire()
+    {
+        //spawns aoe damage around a spawn point randomly
+
+        yield return new WaitForSeconds(interval);
+
+        while (true)
+        {
+            for (int i = 0; i < density; i++)
+            {
+                Vector2 spawnPosition = new Vector2(firePoint.position.x + Random.Range(-aoeSpread, aoeSpread), firePoint.position.y + Random.Range(-aoeSpread, aoeSpread));
+                Instantiate(aoeP, spawnPosition, firePoint.rotation);
+            }
+            yield return new WaitForSeconds(interval);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Walls"))
+        {
+            //destroy this object hitting walls
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.CompareTag("Knight") && Input.GetAxis("Jump") > 0)
+        {
+            //do not destroy player, destroy this object
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.CompareTag("Rogue") && Input.GetAxis("Jump") > 0)
+        {
+            //do not destroy player, destroy this object
+            Destroy(gameObject);
+        }
+        //else if (other.gameObject.CompareTag("Enemy"))
+        //{
+        //    //no enemy friendly fire, destroy this object
+        //    Destroy(gameObject);
+        //}
+        else if (other.gameObject.CompareTag("Rogue") || other.gameObject.CompareTag("Knight") || other.gameObject.CompareTag("Barbarian"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(5);
+            Destroy(gameObject);
+        }
+    }
+}
