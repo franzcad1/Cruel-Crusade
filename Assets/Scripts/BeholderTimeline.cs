26f266b9f40da6d0d60940d113892142295ba929
pinct <pinctcourneya@gmail.com>
2020-12-03 13:54:26 -0500

    Built Beholder Timeline

---
+++ Assets/Scripts/BeholderTimeline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BeholderTimeline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Knight") || collision.CompareTag("Rogue") || collision.CompareTag("Barbarian"))
+        {
+            GetComponent<PlayableDirector>().enabled = true;
+        }
+    }
+}
