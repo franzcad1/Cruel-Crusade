a7d5a5278e19441ff5f1ac5b9ca4e61f63c92644
Edgar <edgaradrian.mejiarazo@gmail.com>
2020-12-03 21:26:34 -0500

    Sword Sounds

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-   
+    AudioSource sword;
     private bool hasEnemy = false;
     [SerializeField] private GameObject enemy;
 
     void Start()
     {
-        
+        sword = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         if (enemy != null && Input.GetMouseButtonDown(0))
         {
             Destroy(enemy);
+=======
+        if (Input.GetMouseButtonDown(0))
+        {
+            sword.Play();
+>>>>>>> Stashed changes
         }
     }
 
@@ -26,14 +32,34 @@ public class Attack : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy") && !collision.isTrigger)
         {
+<<<<<<< Updated upstream
             enemy = collision.gameObject;
+=======
+            
+            if (Input.GetAxis("Jump") > 0 && collision.gameObject.CompareTag("Knight"))
+            {
+                ////Temporary Don´t destroy while Knight is in Shield 
+                /////to be replaced with 75% less damage rutine
+                
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+            }
+            
+>>>>>>> Stashed changes
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && !collision.isTrigger)
         {
+<<<<<<< Updated upstream
             enemy = collision.gameObject;
+=======
+            
+            Destroy(collision.gameObject);
+>>>>>>> Stashed changes
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
